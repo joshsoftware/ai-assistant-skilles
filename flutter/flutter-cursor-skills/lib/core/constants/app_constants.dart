ae843a72cecf20bffb48782905bbe3314421fc9c
Amit Rawal <amit.rawal@joshsoftware.com>
2026-06-10 13:23:02 +0530

    project removed

--- flutter/flutter-cursor-skills/lib/core/constants/app_constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  AppConstants._();
-
-  static const String appName = 'BFSI App';
-  static const String tokenKey = 'auth_token';
-  static const String refreshTokenKey = 'refresh_token';
-  static const String userIdKey = 'user_id';
-  static const int sessionTimeoutMinutes = 30;
-}
--- flutter/flutter-cursor-skills/lib/features/dashboard/domain/repositories/dashboard_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:bfsi_app/core/error/failures.dart';
-import 'package:bfsi_app/features/dashboard/domain/entities/account_entity.dart';
-
-abstract class DashboardRepository {
-  Future<Either<Failure, List<AccountEntity>>> getAccounts();
-}
